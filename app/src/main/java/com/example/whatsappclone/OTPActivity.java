cebaccc5e643517d9eb5559cc199bb1ddcafc592
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2021-03-22 19:50:20 +0530

    Phone Verification Completed

--- app/src/main/java/com/example/whatsappclone/OTPActivity.java
+++ app/src/main/java/com/example/whatsappclone/OTPActivity.java
@@ -1,14 +1,101 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.whatsappclone.databinding.ActivityOTPBinding;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.mukesh.OnOtpCompletionListener;
+
+import java.util.concurrent.TimeUnit;
 
 public class OTPActivity extends AppCompatActivity {
 
+    ActivityOTPBinding binding;
+    FirebaseAuth auth;
+
+    String verificationID;
+
+    ProgressDialog dialog;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_o_t_p);
+        binding = ActivityOTPBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Sending OTP ...");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        String phoneNumber = getIntent().getStringExtra("phoneNumber");
+
+        binding.phoneLabel.setText("Verify  " + phoneNumber);
+
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verifiedID, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verifiedID, forceResendingToken);
+
+                        verificationID = verifiedID;
+                        dialog.dismiss();
+                    }
+                })
+                .build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        binding.otpView.setOnClickListener(view -> {
+
+        });
+
+        binding.otpView.setOtpCompletionListener(otp -> {
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationID, otp);
+
+            auth.signInWithCredential(credential).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(getApplicationContext(), PhoneNumberActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/whatsappclone/PhoneNumberActivity.java
+++ app/src/main/java/com/example/whatsappclone/PhoneNumberActivity.java
@@ -2,13 +2,37 @@ package com.example.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.whatsappclone.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        binding.nameGet.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), OTPActivity.class);
+            intent.putExtra("phoneNumber", binding.phoneBox.getText().toString());
+            finish();
+            startActivity(intent);
+        });
+
     }
 }
