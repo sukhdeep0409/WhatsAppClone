4f67fe2d8ed5f5d76672a297738a3f90f7a34631
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2021-03-26 18:44:08 +0530

    Layout for chat activity

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search :
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.invite :
+                Toast.makeText(this, "Invite Clicked", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.settings:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
